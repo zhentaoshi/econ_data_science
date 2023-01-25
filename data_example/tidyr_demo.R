a3891ea605f0b78cefb9d736b3c3434bc9e9d971
Zhentao <shizhentao@gmail.com>
2023-01-25 23:42:26 +0800

    embed data transformation into two slides

--- data_example/tidyr_demo.R
+++ data_example/tidyr_demo.R
@@ -37,3 +37,28 @@ g1 <- s1 %>% gather('1950':'2019', key = "year", value = "pop")
 
 ######################
 
+# load("data_example/big150.Rdata")
+load("big150.Rdata")
+
+
+big150_1 <- big150 %>% select(typb, b1, b1_c) %>%
+            gather("b1", "b1_c", key = "estimator", value = "value")
+print(head(big150_1))
+
+
+
+
+p1 <- ggplot(big150_1)
+p1 <- p1 + geom_area(
+  stat = "density", alpha = .25,
+  aes(x = value, fill = estimator), position = "identity"
+)
+p1 <- p1 + facet_grid(. ~ typb)
+p1 <- p1 + geom_vline(xintercept = 0)
+p1 <- p1 + theme_bw()
+p1 <- p1 + theme(
+  strip.text = element_text(size = 12),
+  axis.text = element_text(size = 12)
+)
+print(p1)
+
